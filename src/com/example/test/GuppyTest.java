ff753983e9dff9e29bd044682664ccf49ccceed3
stevannohero <stevannohero@gmail.com>
2018-04-25 21:00:52 +0700

    init test guppy

--- src/com/example/test/GuppyTest.java
+++ src/com/example/test/GuppyTest.java
@@ -1,92 +1,21 @@
 package com.example.test;
 
+import com.example.model.Guppy;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GuppyTest {
 
-    @Test
-    public void move() {
-    }
+    Guppy guppy = new Guppy();
 
     @Test
-    public void setTarget() {
-    }
-
-    @Test
-    public void setTarget1() {
-    }
-
-    @Test
-    public void isHungry() {
-    }
-
-    @Test
-    public void isDeadByStarvation() {
-    }
-
-    @Test
-    public void isSpawningCoin() {
-    }
-
-    @Test
-    public void increaseHunger() {
-    }
-
-    @Test
-    public void increaseSpawningCoin() {
-    }
-
-    @Test
-    public void isTimeToDecideYet() {
-    }
-
-    @Test
-    public void isNearTargetYet() {
-    }
-
-    @Test
-    public void getHunger() {
-    }
-
-    @Test
-    public void getState() {
-    }
-
-    @Test
-    public void getSpawnedCoinValue() {
-    }
-
-    @Test
-    public void setTargetFood() {
-    }
-
-    @Test
-    public void hasEaten() {
-    }
-
-    @Test
-    public void hasSpawnedCoin() {
-    }
-
-    @Test
-    public void isOnRemoval() {
-    }
-
-    @Test
-    public void hasBeenSold() {
-    }
-
-    @Test
-    public void hasBeenEaten() {
-    }
-
-    @Test
-    public void value() {
-    }
-
-    @Test
-    public void isFacingRight() {
+    public void testGuppy() {
+        assertNotNull("Guppy ctor TEST FAIL", guppy);
+        assertEquals("Guppy's hunger TEST FAIL", 0, guppy.getHunger());
+        assertEquals("Guppy's state TEST FAIL",1, guppy.getState());
+        guppy.hasEaten();
+        guppy.hasEaten();
+        assertEquals("Guppy's state TEST FAIL",3, guppy.getState());
     }
 }
